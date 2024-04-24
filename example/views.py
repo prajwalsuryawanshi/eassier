394a2d4ea1ac8dfaf6aade8b2f51370d6ffa46e8
Prajwal Suryawanshi <152725837+prajwalsuryawanshi@users.noreply.github.com>
2024-04-24 10:19:08 +0530

    Update views.py

--- example/views.py
+++ example/views.py
@@ -8,8 +8,8 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <h1>Hello from prajwal </h1>
+            <p>The current time is { now }. <br>the app will be deployed sooon!</p>
         </body>
     </html>
     '''
